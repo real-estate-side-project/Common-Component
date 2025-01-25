20cf2cdc8b53e1e8247589609468e6d70633a8be
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2025-01-25 10:27:44 +0900

    feat: 버튼 컴포넌트 수정

--- src/app/buttons/page.tsx
+++ src/app/buttons/page.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/Buttons/Button';
 import SingleIconButton from '@/components/Buttons/SingleIconButton';
+import TextButton from '@/components/Buttons/TextButton';
 import { MdOutlineGridView } from 'react-icons/md';
 
 const page = () => {
@@ -38,6 +39,16 @@ const page = () => {
                         <Button priority={'tertiary'} isDisabled />
                         <Button size={'lg'} priority={'tertiary'} isDisabled />
                     </div>
+                    <div className="flex gap-2 items-center pb-2">
+                        <Button size={'sm'} priority={'gray'} />
+                        <Button priority={'gray'} />
+                        <Button size={'lg'} priority={'gray'} />
+                    </div>
+                    <div className="flex gap-2 items-center pb-2">
+                        <Button size={'sm'} priority={'gray'} isDisabled />
+                        <Button priority={'gray'} isDisabled />
+                        <Button size={'lg'} priority={'gray'} isDisabled />
+                    </div>
                 </div>
                 <div className="flex flex-col">
                     <h1 className="text-3xl font-bold pb-1 text-white">Left Icon</h1>
@@ -70,6 +81,16 @@ const page = () => {
                         <Button priority={'tertiary'} size={'sm'} isDisabled icon={<MdOutlineGridView />} />
                         <Button priority={'tertiary'} isDisabled icon={<MdOutlineGridView />} />
                         <Button priority={'tertiary'} size={'lg'} isDisabled icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-2 items-center pb-2">
+                        <Button priority={'gray'} size={'sm'} icon={<MdOutlineGridView />} />
+                        <Button priority={'gray'} icon={<MdOutlineGridView />} />
+                        <Button priority={'gray'} size={'lg'} icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-2 items-center pb-2">
+                        <Button priority={'gray'} size={'sm'} isDisabled icon={<MdOutlineGridView />} />
+                        <Button priority={'gray'} isDisabled icon={<MdOutlineGridView />} />
+                        <Button priority={'gray'} size={'lg'} isDisabled icon={<MdOutlineGridView />} />
                     </div>
                 </div>
             </div>
@@ -113,7 +134,7 @@ const page = () => {
                                 <SingleIconButton priority={'tertiary'} icon={<MdOutlineGridView />} />
                                 <SingleIconButton priority={'tertiary'} size={'lg'} icon={<MdOutlineGridView />} />
                             </div>
-                            <div className="flex gap-2 items-center">
+                            <div className="flex gap-2 items-center pb-2">
                                 <SingleIconButton
                                     priority={'tertiary'}
                                     size={'sm'}
@@ -128,7 +149,95 @@ const page = () => {
                                     icon={<MdOutlineGridView />}
                                 />
                             </div>
+                            <div className="flex gap-2 items-center pb-2">
+                                <SingleIconButton priority={'gray'} size={'sm'} icon={<MdOutlineGridView />} />
+                                <SingleIconButton priority={'gray'} icon={<MdOutlineGridView />} />
+                                <SingleIconButton priority={'gray'} size={'lg'} icon={<MdOutlineGridView />} />
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <SingleIconButton
+                                    priority={'gray'}
+                                    size={'sm'}
+                                    isDisabled
+                                    icon={<MdOutlineGridView />}
+                                />
+                                <SingleIconButton priority={'gray'} isDisabled icon={<MdOutlineGridView />} />
+                                <SingleIconButton
+                                    priority={'gray'}
+                                    size={'lg'}
+                                    isDisabled
+                                    icon={<MdOutlineGridView />}
+                                />
+                            </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-row border-2 p-6 gap-10">
+                <div className="flex flex-col">
+                    <h1 className="text-3xl font-bold pb-1">Text Button</h1>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} />
+                        <TextButton />
+                        <TextButton size={'lg'} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} isDisabled />
+                        <TextButton isDisabled />
+                        <TextButton size={'lg'} isDisabled />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} priority={'secondary'} />
+                        <TextButton priority={'secondary'} />
+                        <TextButton size={'lg'} priority={'secondary'} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} priority={'secondary'} isDisabled />
+                        <TextButton priority={'secondary'} isDisabled />
+                        <TextButton size={'lg'} priority={'secondary'} isDisabled />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} priority={'tertiary'} />
+                        <TextButton priority={'tertiary'} />
+                        <TextButton size={'lg'} priority={'tertiary'} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} priority={'tertiary'} isDisabled />
+                        <TextButton priority={'tertiary'} isDisabled />
+                        <TextButton size={'lg'} priority={'tertiary'} isDisabled />
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <h1 className="text-3xl font-bold pb-1 text-white">Left Icon</h1>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} icon={<MdOutlineGridView />} />
+                        <TextButton icon={<MdOutlineGridView />} />
+                        <TextButton size={'lg'} icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton size={'sm'} isDisabled icon={<MdOutlineGridView />} />
+                        <TextButton isDisabled icon={<MdOutlineGridView />} />
+                        <TextButton size={'lg'} isDisabled icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton priority={'secondary'} size={'sm'} icon={<MdOutlineGridView />} />
+                        <TextButton priority={'secondary'} icon={<MdOutlineGridView />} />
+                        <TextButton priority={'secondary'} size={'lg'} icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton priority={'secondary'} size={'sm'} isDisabled icon={<MdOutlineGridView />} />
+                        <TextButton priority={'secondary'} isDisabled icon={<MdOutlineGridView />} />
+                        <TextButton priority={'secondary'} size={'lg'} isDisabled icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton priority={'tertiary'} size={'sm'} icon={<MdOutlineGridView />} />
+                        <TextButton priority={'tertiary'} icon={<MdOutlineGridView />} />
+                        <TextButton priority={'tertiary'} size={'lg'} icon={<MdOutlineGridView />} />
+                    </div>
+                    <div className="flex gap-3 items-center pb-3">
+                        <TextButton priority={'tertiary'} size={'sm'} isDisabled icon={<MdOutlineGridView />} />
+                        <TextButton priority={'tertiary'} isDisabled icon={<MdOutlineGridView />} />
+                        <TextButton priority={'tertiary'} size={'lg'} isDisabled icon={<MdOutlineGridView />} />
                     </div>
                 </div>
             </div>
